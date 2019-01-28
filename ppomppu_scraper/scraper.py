ce28f1b61a7971c75553bf0cc1a80d0ece36bc43
lovit <soy.lovit@gmail.com>
2019-01-28 16:47:27 +0900

    Extract unique url

--- ppomppu_scraper/scraper.py
+++ ppomppu_scraper/scraper.py
@@ -10,6 +10,7 @@ def yield_parsed_page(id_, divpage, max_page=3, sleep=1.0, debug=False):
         soup = get_soup(url)
         links = soup.select('tr[class=list0] a[href^=view.php?]')
         urls = ['http://www.ppomppu.co.kr/zboard/%s' % a.attrs.get('href', '').replace('&amp;', '') for a in links]
+        urls = list(set(urls))
         if debug:
             urls = urls[:5]
         for url in urls:
