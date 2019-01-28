9911eea1a554004e6f5c6fd74506f5adc4e71f77
lovit <soy.lovit@gmail.com>
2019-01-28 16:39:10 +0900

    Rename functions

--- ppomppu_scraper/__init__.py
+++ ppomppu_scraper/__init__.py
@@ -5,3 +5,4 @@ from .utils import strf_to_datetime
 from .utils import news_dateformat
 from .utils import user_dateformat
 from .parser import parse_page
+from .scraper import yield_parsed_page
--- ppomppu_scraper/scraper.py
+++ ppomppu_scraper/scraper.py
@@ -2,15 +2,18 @@ import time
 from .utils import get_soup
 from .parser import parse_page
 
+base = 'http://www.ppomppu.co.kr/zboard/zboard.php?id={}&divpage={}&page={}'
 
-def yield_page(id_, divpage, base, max_page=3, sleep=1.0):
+def yield_parsed_page(id_, divpage, max_page=3, sleep=1.0, debug=False):
     for page in range(1, max_page + 1):
         url = base.format(id_, divpage, page)
         soup = get_soup(url)
         links = soup.select('tr[class=list0] a[href^=view.php?]')
         urls = ['http://www.ppomppu.co.kr/zboard/%s' % a.attrs.get('href', '').replace('&amp;', '') for a in links]
+        if debug:
+            urls = urls[:5]
         for url in urls:
-            page = parse_page(url)
-            yield page
+            information = parse_page(url)
+            yield information
             time.sleep(sleep)
         print('scrap from {} / {} pages'.format(page, max_page))
