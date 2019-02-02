28df24f91a0ba9d48e5e1f32b13f113a96760875
lovit <soy.lovit@gmail.com>
2019-02-02 23:59:38 +0900

    BeautifulSoup4 4.6.x -> 4.7.x 대응

--- ppomppu_scraper/scraper.py
+++ ppomppu_scraper/scraper.py
@@ -8,7 +8,7 @@ def yield_parsed_page(id_, divpage, max_page=3, sleep=1.0, debug=False):
     for page in range(1, max_page + 1):
         url = base.format(id_, divpage, page)
         soup = get_soup(url)
-        links = soup.select('tr[class=list0] a[href^=view.php?]')
+        links = soup.select('tr[class=list0] a[href^="view.php?"]')
         urls = ['http://www.ppomppu.co.kr/zboard/%s' % a.attrs.get('href', '').replace('&amp;', '') for a in links]
         urls = list(set(urls))
         if debug:
